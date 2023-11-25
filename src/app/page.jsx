8a70e97360e19b0344139b97243ce7658452f251
Rhon-Vincent <134147488+Rhon-Vincent@users.noreply.github.com>
2023-11-26 06:30:23 +0800

    New update
    Landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,28 @@
-import Hero from '../components/Hero'
-import Navbar from "../components/Navbar"
-import PopularProducts from "../components/PopularProducts"
-import Items from "../components/items"
+import React from "react";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import About from "../components/About";
+import Items from "../components/items";
+import Vision from "../components/Vision";
+
+// import AboutUs from "./LandingPage/AboutUs";
+// import Header from "./LandingPage/Header";
+// import Hero from "./LandingPage/Hero";
+// import Mission from "./LandingPage/Mission";
+// import Vision from "./LandingPage/Vision";
+
 export default function Home() {
   return (
     <main>
       <Navbar />
-
       {/* HERO */}
       <Hero />
-
-      {/*  Categories */}
-      <PopularProducts />
-
+      {/* About us */}
+      <About />
       {/* Items */}
       <Items />
-
+      {/* Vision */}
+      <Vision />
     </main>
-  )
+  );
 }
